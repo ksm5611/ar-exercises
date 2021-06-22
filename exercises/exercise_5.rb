75bb0d8fd624d58d8c8332148455db2da76a2b7c
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-06-22 07:41:51 +0000

    store and employee

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_annual_revenue = Store.average(:annual_revenue)
+annual_sales_more_1M = Store.where(annual_revenue: 1000000..-Float::INFINITY).count
+
+puts total_revenue
+puts average_annual_revenue
+puts annual_sales_more_1M
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.find_by(id: 1)
+store2 = Store.find_by(id: 2)
+store4 = Store.find_by(id: 4)
+store5 = Store.find_by(id: 5)
+store6 = Store.find_by(id: 6)
+
+
+store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+store2.employees.create(first_name: "sumin", last_name: "kim", hourly_rate: 260)
+store4.employees.create(first_name: "sura", last_name: "jeon", hourly_rate: 600)
+store5.employees.create(first_name: "hali", last_name: "Lee", hourly_rate: 620)
+store6.employees.create(first_name: "HOHO", last_name: "you", hourly_rate: 160)
